51be86157a1e0b43fc272dfd3f45755607a997d8
WilloIzCitron(AetherLumine) <49786146+WilloIzCitron@users.noreply.github.com>
2024-05-22 12:09:45 +0700

    i win

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const Humanoid = require("humanoid-js");
 const cheerio = require('cheerio');
 const express = require('express');
 const fs = require('fs');
+request = require('request');
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
 let humanoid = new Humanoid();
 const writeStream = fs.createWriteStream('scraped.csv');
 
@@ -13,7 +24,9 @@ humanoid.get("https://lahelu.com/post/PxW2Y04t2") // link target
 .then(res => {
         const $ = cheerio.load(res.body);
         let linkImg = $("head").find(`[property="og:image"]`).attr('content'); // get via property from head meta
-        writeStream.write(`\nlinkImg: ${linkImg}`)
+        writeStream.write(`linkImg: ${linkImg}`)
+        download(linkImg, 'scraped/fetched.png', function(){
+            console.log('Downloaded Scraped Image');})
 }).catch(err => console.error(err))
 
 app.listen(port, () => {
